157a9f51ca77dd9181fc42faf17a276933d5bad4
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-04-23 16:46:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let greet =(name)=>  `Hello, ${name} how are you doing today?`
+
+
+/*
+8kyu Returning Strings
+
+
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
